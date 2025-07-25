6485f2c141d141a7808b1bef009e39bc5a380e36
ZikXewen <datasup12@gmail.com>
2025-07-24 22:36:10 -0500

    fix prod

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as path from "path";
 
 // https://vitejs.dev/config
 export default defineConfig({
-  base: "./",
-  root: path.join(__dirname, "src", "renderer"),
   plugins: [react()],
 });
