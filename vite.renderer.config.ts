7b00b278f47b7753d81de616a05edb94a509fc5f
ZikXewen <datasup12@gmail.com>
2025-07-24 21:48:51 -0500

    organize

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+  base: "./",
+  root: path.join(__dirname, "src", "renderer"),
+  plugins: [react()],
+});
